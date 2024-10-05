47bb77341950f4b6646cdc5776232ee3998c136f
minhoshin11 <tlsalsgh777@naver.com>
2024-10-06 04:28:20 +0900

    프로젝트 카드 뒷면 값 수정

--- src/app/_components/ProjectSection/component/Card.tsx
+++ src/app/_components/ProjectSection/component/Card.tsx
@@ -30,54 +30,51 @@ export default function Card({ project }: CardProps) {
           </div>
 
           {/* 카드 뒷면 */}
-          <div className="text-xl absolute w-full h-full py-2 px-4 bg-black text-white opacity-0 group-hover:opacity-85 transition-opacity duration-500 flex flex-col justify-between items-center text-center overflow-hidden rounded-lg">
-            <div className="overflow-y-auto h-full w-full py-4 px-2">
+          <div className="text-xl absolute w-full h-full pt-4 px-4 bg-black text-white opacity-0 group-hover:opacity-85 transition-opacity duration-500 flex flex-col justify-between items-center text-center overflow-hidden rounded-lg">
+            <div className="overflow-y-auto flex flex-col h-full w-full gap-3 py-2 px-2">
               <h2 className="text-2xl">{project.title}</h2>{" "}
               {/* 글자 크기 키움 */}
-              <p className=" mt-1 md:mt-2">{project.subtitle}</p>{" "}
-              {/* 프로젝트 링크 */}
-              <div className="mt-3">
-                <h3 className="font-bold text-base">프로젝트 링크</h3>{" "}
-                {/* 글자 크기 키움 */}
-                <ul className="flex flex-col md:gap-1 text-xl">
-                  {project.url.github && (
-                    <li>
-                      <a
-                        href={project.url.github}
-                        className="text-blue-500"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        GitHub
-                      </a>
-                    </li>
-                  )}
-                  {project.url.video && (
-                    <li>
-                      <a
-                        href={project.url.video}
-                        className="text-blue-500"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        영상
-                      </a>
-                    </li>
-                  )}
-                  {project.url.site && (
-                    <li>
-                      <a
-                        href={project.url.site}
-                        className="text-blue-500"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        배포주소
-                      </a>
-                    </li>
-                  )}
-                </ul>
-              </div>
+              <p>{project.subtitle}</p> {/* 프로젝트 링크 */}
+              <h3 className="font-bold text-base">{project.period}</h3>{" "}
+              {/* 글자 크기 키움 */}
+              <ul className="flex flex-col gap-1 text-xl">
+                {project.url.github && (
+                  <li>
+                    <a
+                      href={project.url.github}
+                      className="text-blue-500"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      GitHub
+                    </a>
+                  </li>
+                )}
+                {project.url.video && (
+                  <li>
+                    <a
+                      href={project.url.video}
+                      className="text-blue-500"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      영상
+                    </a>
+                  </li>
+                )}
+                {project.url.site && (
+                  <li>
+                    <a
+                      href={project.url.site}
+                      className="text-blue-500"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      배포주소
+                    </a>
+                  </li>
+                )}
+              </ul>
             </div>
           </div>
         </div>
